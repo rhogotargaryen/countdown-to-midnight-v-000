09a281a7069ccf54d819796949c82bd0525fee72
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 05:25:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -9,9 +9,10 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
-  while seconds != -1
+  while seconds > 0
     puts "#{seconds} SECOND(S)!"
     sleep(1)
     seconds -= 1
   end
+  return "HAPPY NEW YEAR!"
 end
